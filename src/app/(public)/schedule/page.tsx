a34c97264c7b1cfeda8ebb4646ac2a1e7f5e9dbb
Vlad Lesun <vladlesunmma@gmail.com>
2026-03-21 17:12:37 +0300

    feat: adaptive to schedule page

--- src/app/(public)/schedule/page.tsx
+++ src/app/(public)/schedule/page.tsx
@@ -7,23 +7,23 @@ const Schedule = () => {
 	const scheduleContent = siteConfig.pagesContent.schedule.plansSection;
 
 	return (
-		<section className='pt-[114px] pb-[220px] bg-[#1f0808]'>
+		<section className='pt-[114px] pb-[220px] bg-[#1f0808] max-lg:py-[114px] max-sm:py-[64px]'>
 			<div className='max-w-[980px] mx-auto px-7'>
 				<h2 className='max-w-100 mb-9 text-[32px] leading-[109%] tracking-[-0.05em]'>
 					{scheduleContent.title}
 				</h2>
 
-				<ul className='mb-[220px] border-b-[0.5px] border-[#fff]'>
+				<ul className='mb-[220px] border-b-[0.5px] border-[#fff] max-lg:mb-[114px] max-sm:mb-[56px]'>
 					{scheduleContent.list.map(item => (
 						<li
 							key={item.number}
-							className='flex justify-between items-center py-[50px] border-t-[0.5px] border-[#fff]'
+							className='flex justify-between items-center py-[50px] border-t-[0.5px] border-[#fff] max-sm:flex-col max-sm:items-start'
 						>
-							<p className='text-[300px] font-extralight leading-[135%] tracking-[-0.02em]'>
+							<p className='text-[300px] font-extralight leading-[135%] tracking-[-0.02em] max-lg:text-[200px] max-sm:text-[100px]'>
 								{item.number}
 							</p>
 
-							<div className='w-[50%]'>
+							<div className='w-[50%] max-lg:w-full max-lg:max-w-[481px]'>
 								<h3 className='mb-[56px] text-lg font-medium'>{item.title}</h3>
 								<p className='text-lg'>{item.text}</p>
 							</div>
@@ -31,8 +31,8 @@ const Schedule = () => {
 					))}
 				</ul>
 
-				<div className='flex items-center gap-[100px]'>
-					<p className='text-[32px] leading-[109%] tracking-[-0.05em]'>
+				<div className='flex flex-wrap justify-between items-center gap-[56px]'>
+					<p className='text-[32px] leading-[109%] tracking-[-0.05em] max-sm:text-[22px]'>
 						{scheduleContent.preButton}
 					</p>
 
